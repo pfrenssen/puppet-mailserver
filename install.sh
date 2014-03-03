e39bd8a650f57dea215be8fe3e2a657ca324b2eb
Pieter Frenssen <pieter@frenssen.be>
2014-03-03 22:03:18 +0100

    Sudo is not yet installed.

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 
 # Add the repository.
 wget https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb -O ~/puppetlabs-release-wheezy.deb
-sudo dpkg -i ~/puppetlabs-release-wheezy.deb
-sudo apt-get update
-sudo apt-get install puppet
+dpkg -i ~/puppetlabs-release-wheezy.deb
+apt-get update
+apt-get install puppet
 rm ~/puppetlabs-release-wheezy.deb
 
