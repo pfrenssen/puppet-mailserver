60ed0282306ded41fb7dafc5d410993acba86e8c
Pieter Frenssen <pieter@frenssen.be>
2014-03-09 00:00:47 +0100

    Install librarian-puppet.

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 #
-# Installs puppet on Debian Wheezy.
+# Installs puppet and librarian-puppet on Debian Wheezy.
 #
 
 # Add the repository.
 wget https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb -O ~/puppetlabs-release-wheezy.deb
 dpkg -i ~/puppetlabs-release-wheezy.deb
+
+# Install packages.
 apt-get update
-apt-get install puppet
+apt-get install puppet ruby-dev
+
+# Install librarian-puppet.
+gem install librarian-puppet
+
+# Clean up.
 rm ~/puppetlabs-release-wheezy.deb
 
